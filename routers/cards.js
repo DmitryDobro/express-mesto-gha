53f1e7fc2cbd1c8477a12669ce6c50ecb7787f91
Dima Dobronovich <dobronovich_95@mail.ru>
2024-01-23 18:03:08 +0700

    fix

--- routers/cards.js
+++ routers/cards.js
@@ -7,7 +7,7 @@ const { validateCreateCard, validateCardId } = require('../utils/validator');
 cardRouter.get('/', getCards);
 cardRouter.post('/', validateCreateCard, createCard);
 cardRouter.get('/:cardId', validateCardId, getCardById);
-cardRouter.put('/:cardId/likes', putLikeCard);
+cardRouter.put('/:cardId/likes', validateCardId, putLikeCard);
 cardRouter.delete('/:cardId/likes', validateCardId, deleteLikeCard);
 cardRouter.delete('/:cardId', validateCardId, deleteCard);
 module.exports = cardRouter;
